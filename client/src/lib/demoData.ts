650b72ef8f593636880263a8d442fd8084603dce
Dr. Sam Ikoku <s.ikoku@nakachiconsulting.com.ng>
2026-09-15 20:38:46 +0100

    Add comprehensive demo data exports - build passing

--- client/src/lib/demoData.ts
+++ client/src/lib/demoData.ts
@@ -1,5 +1,7 @@
 export const DEMO_PORTFOLIO = { value: 0, investments: [] }
-export const demoPortfolioData = { portfolioValue: 0, investments: [], returns: 0 }
-export const demoDashboardStats = { totalInvested: 0, activeInvestments: 0, monthlyReturns: 0, yearlyGrowth: 0 }
+export const DEMO_INVESTMENTS = []
+export const DEMO_PERFORMANCE_DATA = []
 export const DEMO_TRANSACTIONS = []
 export const DEMO_ALERTS = []
+export const demoPortfolioData = { portfolioValue: 0, investments: [], returns: 0 }
+export const demoDashboardStats = { totalInvested: 0, activeInvestments: 0, monthlyReturns: 0, yearlyGrowth: 0 }
